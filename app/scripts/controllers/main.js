3b1013bf3a3f6bf5102ecc6ef2d575f179b25057
jessefranklin <jessefrankling@gmail.com>
2014-02-10 14:26:49 -0500

    Map plugin removal in progress. Google maps with title

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,47 +8,57 @@ app.controller('MainCtrl', ['$rootScope', '$scope', '$location', 'geolocation', 
 			lat:data.coords.latitude,
 			long:data.coords.longitude
 		};
-
-		$scope.map.center = {
-			latitude: data.coords.latitude,
-			longitude: data.coords.longitude
-		};
 	});
 	
 	var jobList = jobs;
 	
 	jobList.then(function(data){
 		$scope.jobs = data.data;
+	});
 
-		for(var i = 0, iLength = data.data.length; i < iLength; i++) {
-			$scope.map.markers.push({
-				name: $scope.jobs[i].serviceDesc,
-				latitude: $scope.jobs[i].location.lat,
-				longitude: $scope.jobs[i].location.long,
-				title: $scope.jobs[i].serviceName,
-				id : $scope.jobs[i]._id
+	var mapOptions = {
+		zoom: 14,
+		center: new google.maps.LatLng(40.0000, -98.0000),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	$scope.markers = [];
+
+	var infoWindow = new google.maps.InfoWindow(),
+		image = '../img/money.png',
+		createMarker = function (info){
+			var marker = new MarkerWithLabel({
+	            map: $scope.map,
+	            position: new google.maps.LatLng(info.location.lat, info.location.long),
+	            title: info.name,
+	            labelContent: info.name,
+				labelAnchor: new google.maps.Point(22, 0),
+				labelClass: 'labels',
+				labelStyle: {opacity: 0.75}
 			});
-			//$scope.map.click.push('gotoDetailView($scope.jobs[i]._id)');
+			marker.content = '<div class="infoWindowContent">' + info.serviceName + '</div>';
+        
+			google.maps.event.addListener(marker, 'click', function(){
+				infoWindow.setContent('<h2>'+info.serviceName+'</h2>'+ info.serviceDesc +'<br /><a href="#/detail/'+info._id+'" class="button">Click</a>');
+				infoWindow.open($scope.map, marker);
+			});
+        
+			$scope.markers.push(marker);
+    };
+    
+	jobList.then(function(data){
+		$scope.jobs = data.data;
+		for (var i = 0, iLength = data.data.length; i < iLength; i++){
+			createMarker($scope.jobs[i]);
 		}
 	});
 
-	$scope.map = {
-			center: {
-				latitude: 44,
-				longitude: -75
-			},
-			markers:[],
-			zoom: 14
-		};
-
-
-	$scope.onMarkerClicked = function (id) {
-		//marker.showWindow = true;
-		$location.path('/detail/'+id);
-		//$scope.marker.showWindow = true;
-        //window.alert("Marker: lat: " + marker.latitude + ", lon: " + marker.longitude + " clicked!!")
+	$scope.openInfoWindow = function(e, selectedMarker){
+		e.preventDefault();
+		google.maps.event.trigger(selectedMarker, 'click');
 	};
-
 	
 }]);
 
